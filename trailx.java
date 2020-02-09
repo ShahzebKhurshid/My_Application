47c08f3331e765028e047571069c6edfef4d0cea
Boxin-Cao <60851277+Boxin-Cao@users.noreply.github.com>
2020-02-09 13:22:57 -0500

    Add files via upload

---
+++ trailx.java
@@ -0,0 +1,31 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class trailx {
+
+	public static void main(String[] args) {
+	
+		Scanner kb = new Scanner (System.in);
+		String input0;
+		String [] input1; 
+
+		char []trail = {',','.','-','!','?'};
+
+		System.out.println("Please ener a phrase ");
+		input0 = kb.nextLine();
+		input1 = input0.split(" ");
+		System.out.println(input1);
+//for (int a = 0; a< trail.length; a++) {
+//	if 
+	
+	}
+	
+}
+	
+
+
+
+}
+	
+
