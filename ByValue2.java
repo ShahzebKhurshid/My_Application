0b1bbe88863fc98d16af0a349051dacad16e901d
Katherine Chin <k@alumni.bentley.edu>
2020-03-14 20:21:12 -0400

    added ByValue2 java file as a test

---
+++ ByValue2.java
@@ -0,0 +1,26 @@
+/**
+ * ByValue.java demonstrates that arguments are passed by value
+ */
+public class ByValue {
+	public static void main(String[] args) {
+		// x is a local variable in main
+		int x = 3;
+		int z = 0;
+		change(x);
+		System.out.println("in the main method, x = " + x);
+		// add code to change value of x using the update method
+	}
+
+	/** change() adds to the value of the parameter */
+	public static void change(int x) {
+		// x is a local variable in the change method 
+		// NOT the same variable as in the main method
+		x += 5;
+		System.out.println("in the change method, x = " + x);
+	}
+	
+	/** update method adds 5 to the value of the parameter and 
+	 *  returns that value */
+	// your code here
+	System.out.println(z);
+}
